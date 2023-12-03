e062b3eae3925acaeedbe454196c6ed397d1d06c
olga voronets <voronets80@gmail.com>
2023-12-03 20:03:00 +0300

    Добавлена обработка сбора обратной связи в контроллер contacts

--- catalog/views.py
+++ catalog/views.py
@@ -5,4 +5,11 @@ def home(request):
 
 
 def contacts(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+        print(f'Имя клиента: {name}\nКонтактный телефон: {phone}\nСообщение: {message}')
+        # with open('info.txt', 'a', encoding='utf8') as file:
+        #     file.write(f'Имя клиента: {name}\nКонтактный телефон: {phone}\nСообщение: {message}\n')
     return render(request, 'contacts.html')

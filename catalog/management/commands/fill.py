9cbe1e4903aa881d3b6d7fedb4bc07e6fdbc663d
olga voronets <voronets80@gmail.com>
2023-12-06 22:24:25 +0300

    Создана кастомная команда для очищения, сброса счетчика и последующего заполнения базы данных

---
+++ catalog/management/commands/fill.py
@@ -0,0 +1,48 @@
+
+from django.core.management import BaseCommand
+from django.db import connection
+from django.db.models.functions import datetime
+
+from catalog.models import Category, Product
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        Category.objects.all().delete()
+        Product.objects.all().delete()
+        with connection.cursor() as cursor:
+            cursor.execute("ALTER SEQUENCE catalog_category_id_seq RESTART WITH 1;")
+            cursor.execute("ALTER SEQUENCE catalog_product_id_seq RESTART WITH 1;")
+
+        category_list = [
+            {'name': 'Фрукты'},
+            {'name': 'Овощи'},
+            {'name': 'Напитки'},
+            {'name': 'Бакалея'}
+        ]
+        categories_for_create = []
+        for cat in category_list:
+            categories_for_create.append(Category(**cat))
+        Category.objects.bulk_create(categories_for_create)
+
+        vegetable = Category.objects.get(name="Овощи")
+        fruit = Category.objects.get(name="Фрукты")
+        drink = Category.objects.get(name="Напитки")
+        grocery =Category.objects.get(name="Бакалея")
+
+        product_list = [
+            {'name': 'Яблоки', 'description': 'ГренниСмит, 1 кг', 'preview': '', 'category': fruit, 'price': 150},
+            {'name': 'Апельсины', 'description': 'Сочные, 1 кг', 'preview': '', 'category': fruit, 'price': 150},
+            {'name': 'Помидоры', 'description': 'Фламенко, 1 кг', 'preview': '', 'category': vegetable, 'price': 300},
+            {'name': 'Огурцы', 'description': 'Колючие короткоплодные, 1 кг', 'preview': '', 'category': vegetable,'price': 200},
+            {'name': 'Вода', 'description': 'Газированая, 1 шт', 'preview': '', 'category': drink, 'price': 40},
+            {'name': 'Сок', 'description': 'Вишневый 1 шт', 'preview': '', 'category': drink, 'price': 150},
+            {'name': 'Печенье', 'description': 'Овсяное с изюмом, 1 уп', 'preview': '', 'category': grocery, 'price': 100},
+            {'name': 'Зефир', 'description': 'В шоколаде, 1 уп', 'preview': '', 'category': grocery, 'price': 150}
+        ]
+
+        products_for_create = []
+        for prod in product_list:
+            products_for_create.append(Product(**prod))
+        Product.objects.bulk_create(products_for_create)
--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from django.db import models
 
 NULLABLE = {'blank': True, 'null': True}
@@ -6,7 +8,6 @@ NULLABLE = {'blank': True, 'null': True}
 class Category(models.Model):
     name = models.CharField(max_length=100, verbose_name='Категория')
     description = models.TextField(**NULLABLE, verbose_name='Описание')
-
 
     def __str__(self):
         return f'{self.name}'
@@ -17,13 +18,13 @@ class Category(models.Model):
 
 
 class Product(models.Model):
-    name = models.CharField(max_length=250, verbose_name='Наименование')
+    name = models.CharField(max_length=250, verbose_name='Наименование', **NULLABLE)
     description = models.TextField(verbose_name='Описание', **NULLABLE)
     preview = models.ImageField(upload_to='catalog/', verbose_name='Изображение', **NULLABLE)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Категория', **NULLABLE)
-    price = models.IntegerField(verbose_name='Цена за единицу')
-    created_at = models.DateField(verbose_name='Дата создания', **NULLABLE)
-    last_changed_at = models.DateField(verbose_name='Последнее изменение', **NULLABLE)
+    price = models.IntegerField(verbose_name='Цена за единицу', **NULLABLE)
+    created_at = models.DateField(verbose_name='Дата создания', default=datetime.now(), **NULLABLE)
+    last_changed_at = models.DateField(verbose_name='Последнее изменение', default=datetime.now(), **NULLABLE)
 
     def __str__(self):
         return f'{self.name} ({self.category})'

2ecbe30994474b81cc893880a65e98a17332dc30
olga voronets <voronets80@gmail.com>
2023-12-13 14:14:00 +0300

    Созданы контроллер и шаблон для отображения карточки с товаром На главной странице выводится список товаров в цикле Созданы базовый шаблон и шаблонный фильтр для преобразования пути к медиа

--- catalog/views.py
+++ catalog/views.py
@@ -1,7 +1,14 @@
+from catalog.models import Product
 from django.shortcuts import render
 
+
 def home(request):
-    return render(request, 'home.html')
+    context = {
+        'object_list': Product.objects.all(),
+        'title': "Каталог продуктов"
+    }
+    return render(request, 'catalog/home.html', context)
+    # return render(request, 'catalog/home.html')
 
 
 def contacts(request):
@@ -12,4 +19,14 @@ def contacts(request):
         print(f'Имя клиента: {name}\nКонтактный телефон: {phone}\nСообщение: {message}')
         # with open('info.txt', 'a', encoding='utf8') as file:
         #     file.write(f'Имя клиента: {name}\nКонтактный телефон: {phone}\nСообщение: {message}\n')
-    return render(request, 'contacts.html')
+    return render(request, 'catalog/contacts.html')
+
+
+def item(request, pk):
+    # object = Product.objects.get(pk=pk).__dict__
+    context = {
+        'object': Product.objects.get(pk=pk).__dict__,
+        'title': "Карточка продукта",
+
+    }
+    return render(request, 'catalog/item.html', context)

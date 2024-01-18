09167513ac17f6031dded98f59abbc68cbb9c8c7
olga voronets <voronets80@gmail.com>
2024-01-18 21:38:50 +0300

    Добавлено кеширование всего сайта кроме контроллеров создания продукта и создания поста(в приложении blog)

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,4 +1,4 @@
-from django.views.decorators.cache import cache_page
+from django.views.decorators.cache import cache_page, never_cache
 
 from catalog.apps import CatalogConfig
 from django.urls import path
@@ -13,7 +13,7 @@ urlpatterns = [
     path('categories/', CategoryListView.as_view(), name='categories'),
     path('contacts/', contacts, name='contacts'),
     path('<int:pk>/view_product/', cache_page(60)(ProductDetailView.as_view()), name='view_product'),
-    path('create_product/', ProductCreateView.as_view(), name='create_product'),
+    path('create_product/', never_cache(ProductCreateView.as_view()), name='create_product'),
     path('<int:pk>/edit_product/', ProductUpdateView.as_view(), name='edit_product'),
     path('<int:pk>/product/', ProductDeleteView.as_view(), name='delete_product'),
     path('<int:pk>/published/', published_toggle, name='published_toggle')
--- online_store_project/settings.py
+++ online_store_project/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'django.middleware.cache.UpdateCacheMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -54,6 +55,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.cache.FetchFromCacheMiddleware',
 ]
 
 ROOT_URLCONF = 'online_store_project.urls'

5ff1d5deefb0020987228fc5215d312d7043643b
olga voronets <voronets80@gmail.com>
2024-01-22 14:55:54 +0300

    небольшая доработка

--- online_store_project/settings.py
+++ online_store_project/settings.py
@@ -150,7 +150,7 @@ EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 EMAIL_USE_SSL = True
 
 
-CACHE_ENABLED = os.getenv('CACHE_ENABLED') == 'True'
+CACHE_ENABLED = os.getenv('CACHE_ENABLED').lower() in ('true', 'yes', 'y', '1')
 
 if CACHE_ENABLED:
     CACHES = {
